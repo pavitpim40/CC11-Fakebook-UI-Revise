d888d72debef685886c54b0d57803b5f038257d2
pavitpim_MacBookPro <pavitpim@gmail.com>
2022-06-02 23:31:42 +0700

    Routing and Layout entire app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
+import Router from "./routes/Routes"
 function App() {
   return (
-    <div>
-      FakeBook
-      <i class="fa-solid fa-user"></i>
-      <button className="btn btn-primary rounded-md h-12 fw-bold text-4.5">
-        Log In
-      </button>
-    </div>
+   <Router/>
   );
 }
 
